6d38f756256e33b659c15e17bdbaf89b1d2e8e16
andrzej.gorzkiewicz.500 Gorzkiewicz Andrzej <andrzej.gorzkiewicz.500@guest.p.lodz.pl>
2021-02-13 13:58:32 +0100

    front full bez pytań potwierdzających

--- src/main/java/pl/lodz/p/it/spjava/fm/web/specialist/SpecListController.java
+++ src/main/java/pl/lodz/p/it/spjava/fm/web/specialist/SpecListController.java
@@ -58,7 +58,7 @@ public class SpecListController implements Serializable {
             return cancelFaultList();//end of conversation
         } catch (AppBaseException abe) {
             Logger.getLogger(SpecListController.class.getName())
-                    .log(Level.SEVERE, "Zgłoszenie w metodzie akcji assignSpecialist wyjatku typu: ", abe);
+                    .log(Level.SEVERE, "Zgłoszenie w metodzie akcji assignSpecialist wyjatku typu: ", abe); 
             ContextUtils.emitInternationalizedMessage(null, abe.getMessage()); 
             return null;
         }
